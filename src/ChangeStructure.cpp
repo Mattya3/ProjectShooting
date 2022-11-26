22f990f45d724c4d74f67adccda55d46334a6c14
mattya3 <m.tkm1217@ezweb.ne.jp>
2022-11-26 12:29:56 +0900

    ChangeStructureを作成

---
+++ src/ChangeStructure.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include "StructureData.cpp"
+using namespace std;
+
+class CardHas{
+public:
+    
+
+
+    void legisterStructure(vector<int> list){
+        StructureData card;
+        card.writeCardSets(list);
+    }
+};
