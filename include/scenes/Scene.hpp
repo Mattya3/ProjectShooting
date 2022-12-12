e715ac50a03fe17f210ac0772c03ee56344cde9c
Iwa-10 <adgjmp10121@gmail.com>
2022-12-12 12:38:34 +0900

    Fix Scene.hpp
    コンパイル時間の削減のため、宣言と実装を分けた。

--- include/scenes/Scene.hpp
+++ include/scenes/Scene.hpp
@@ -10,45 +10,17 @@ class Scene {
   protected:
     vector<ButtonBase *> btns;
     NextSceneButton *btn_next_scene;
+    virtual void render() {}
 
   public:
     virtual void mouse_button_callback(GLFWwindow *pwin, int button, int action,
-                                       int mods) {
-        double mousex, mousey;
-        if(button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
-            mouse_down = true;
-        } else if(mouse_down && button == GLFW_MOUSE_BUTTON_LEFT &&
-                  action == GLFW_RELEASE) {
-            mouse_down = false;
-            glfwGetCursorPos(pwin, &mousex, &mousey);
-            Setting::to_canonical_xy(mousex, mousey);
-            for(auto &&btn : btns) {
-                if(btn->valid_push_location(mousex, mousey)) {
-                    btn->action_when_pushed();
-                } else {
-                    cout << mousex << ',' << mousey << "\n";
-                }
-            }
-        }
-    }
+                                       int mods);
     virtual void key_callback(GLFWwindow *window, int key, int scancode,
                               int action, int mods) {}
-    virtual void add_button(ButtonBase *btn) { btns.push_back(btn); }
+    virtual inline void add_button(ButtonBase *btn) { btns.push_back(btn); }
     virtual void show_component() {
         for(auto &&e : btns) {
             e->button_view();
-        }
-    }
-    virtual void render(GLFWwindow *window) {
-        while(!glfwWindowShouldClose(window)) {
-            glClear(GL_COLOR_BUFFER_BIT);
-
-            show_component();
-            // btn_func();
-            if(btn_next_scene->is_pushed()) {}
-
-            glfwSwapBuffers(window);
-            glfwPollEvents();
         }
     }
 };
