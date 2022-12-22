4da9829c7fcc45e023ba8ad6118aa3b796b98d5a
Iwa-10 <adgjmp10121@gmail.com>
2022-12-22 23:05:34 +0900

    remove garbage

--- include/GameEngine/README.md
+++
@@ -1,2 +0,0 @@
-## BattleSceneで使うよう
-ll
--- include/scenes/tempCodeRunnerFile.hpp
+++
@@ -1,6 +0,0 @@
-   // glPushMatrix();
-        // glTranslatef(l.sx + l.xlen / 2, l.sy + l.ylen / 2, 0);
-        // glRotatef(rotate, 0, 0, 1);
-        // glTranslatef(-l.sx - l.xlen / 2, -l.sy - l.ylen / 2, 0);
-        // myfighter.view_clone(l);
-        // glPopMatrix();
